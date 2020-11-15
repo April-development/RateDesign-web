88c536958e2d39ed41dfc2f49e369b6617adbda2
Александр Заверняев <zaverclub@gmail.com>
2020-11-15 20:57:15 +0300

    Init

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div><Button>Жми меня</Button></div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { Button } from 'ant-design-vue'
+
+export default Vue.extend({
+  components: {
+    Button,
+  },
+})
+</script>
